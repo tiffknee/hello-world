12820cdf1dd2ed33a036ae817269fda32ba56636
Tiffknee <tcheng@gsd.harvard.edu>
2017-08-04 12:24:04 +0200

    timelapse

---
+++ timelapse.py
@@ -0,0 +1,44 @@
+#ENABLE PYTHON PICAMERA
+# sudo apt-get update
+# sudo apt-get install python-picamera
+
+from time import sleep
+import datetime
+import os
+import picamera
+
+duration = 5 #in hours
+delay = 10 #in minutes
+
+
+folder = datetime.datetime.now().strftime('%Y-%m-%d %H_%M_%S')
+
+os.makedirs(folder)
+os.chdir(folder)
+
+print 'made folder'
+
+
+camera = picamera.PiCamera()
+camera.resolution = (1024, 768)
+
+interval = delay*60 #in secs
+frames = (duration*60*60) / interval #in secs
+
+camera.start_preview()
+
+for i in range(frames):
+    camera.capture('image{0:04d}.jpg'.format(i))
+    print i
+    sleep(interval)
+
+camera.stop_preview()
+
+
+#FOR GIF
+# apt-get install imagemagick -y
+#FOR VIDEO
+# apt-get install ffmpeg
+
+os.system('convert -delay 1 -loop 0 image*.jpg animation.gif')
+print 'animation done'
