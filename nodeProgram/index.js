c8046164568a7c3dfd44c7c69e87462afe5255e1
tiffknee <tiffknee@gmail.com>
2017-12-15 19:08:10 +0100

    commented nodejs code for rpi camera

--- nodeProgram/index.js
+++ nodeProgram/index.js
@@ -4,28 +4,30 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
 var path = require('path');
- 
-var spawn = require('child_process').spawn;
-var proc;
- 
-app.use('/', express.static(path.join(__dirname, 'stream')));
- 
- 
-app.get('/', function(req, res) {
+
+var spawn = require('child_process').spawn; //cache spawn method on child_process
+var proc; //global proc variable that we store the spawned process
+
+
+app.use('/', express.static(path.join(__dirname, 'stream'))); //stream folder = static folder
+
+app.get('/', function(req, res) { //default route to dispatch the index.html
   res.sendFile(__dirname + '/index.html');
 });
- 
-var sockets = {};
- 
+
+
+var sockets = {}; //global sockets object to store all the connected sockets
+
+
 io.on('connection', function(socket) {
  
-  sockets[socket.id] = socket;
+  sockets[socket.id] = socket; //when a client connects to the server, a new socket will be created, stored in the global variable
   console.log("Total clients connected : ", Object.keys(sockets).length);
  
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function() { //delete the disconnected client from the global object
     delete sockets[socket.id];
  
-    // no more sockets, kill the stream
+    // no more clients, kill the stream (save power)
     if (Object.keys(sockets).length == 0) {
       app.set('watchingFile', false);
       if (proc) proc.kill();
@@ -38,7 +40,9 @@ io.on('connection', function(socket) {
   });
  
 });
- 
+
+///////// */ --------- START SERVER ---------  */
+
 http.listen(3000, function() {
   console.log('listening on *:3000');
 });
@@ -53,13 +57,13 @@ function stopStreaming() {
  
 function startStreaming(io) {
  
-  if (app.get('watchingFile')) {
-    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+  if (app.get('watchingFile')) { //if capturing already started, do not re-init the same; emit the last saved image to the client
+    io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000)); //avoid caching
     return;
   }
  
   var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
-  proc = spawn('raspistill', args);
+  proc = spawn('raspistill', args); //if capturing not started, start a new child process and then spawn it with raspistill command; register a watch on the file which changes, whenever the file changes emit a URL to all the connected clients
  
   console.log('Watching for changes...');
  
