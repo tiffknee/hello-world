e9b0dadfcd3d3998a8b7aa419a132c4dc36c4d8b
tiffknee <tiffknee@gmail.com>
2018-01-19 15:03:28 +0100

    socketcam timelapse progress

--- socketcam/index.js
+++ socketcam/index.js
@@ -12,7 +12,32 @@ app.get('/', function (req, res) {
 
 http.listen(3000, function () {
     var busy = false;
+    var imageCount = 0;
+    var delay = 1000 * 60 * 5; //5 minutes
+    var time = 1000 * 60 * 60 * 7; //7 hours
+
     console.log('listening on port 3000');
+
+    var saveFrames = setInterval(function () {
+        campi.getImageAsFile({
+            width: 640,
+            height: 480,
+            nopreview: true,
+            timeout: 1,
+            hflip: true,
+            vflip: true
+        }, './output'+imageCount+'.jpg', function (err) {
+            if (err) {
+                throw err;
+            }
+            console.log('Image #'+imageCount+' captured');
+            imageCount++;
+            if (imageCount === time/delay) {
+                console.log('Captured all frames for timelapse');
+                clearInterval(saveFrames);
+            }
+        });
+    }, delay);
 
     setInterval(function () {
         if (!busy) {
@@ -36,25 +61,6 @@ http.listen(3000, function () {
                     io.sockets.emit('image', message);
                     busy = false;
                 });
-            });
-        }
-    }, 15000);
-
-    setInterval(function () {
-        if (!busy) {
-            busy = true;
-            campi.getImageAsFile({
-                width: 640,
-                height: 480,
-                nopreview: true,
-                timeout: 1,
-                hflip: true,
-                vflip: true
-            }, './output.jpg', function (err) {
-                if (err) {
-                    throw err;
-                }
-                console.log('Image captured');
             });
         }
     }, 15000);
