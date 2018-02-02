c13631338ba47d9c4311c1f60910465d41c890fd
Maria Razzhivina <moorzr@gmail.com>
2018-02-02 12:25:00 +0100

    timelapse also logs sensor data every time a picture is saved

--- socketcam/index.js
+++ socketcam/index.js
@@ -3,6 +3,7 @@ var Campi = require('campi'),
     http = require('http').Server(app),
     io = require('socket.io')(http),
     fs = require('fs'),
+    sensorLib = require('node-dht-sensor'),
     base64 = require('base64-stream');
 
 var campi = new Campi();
@@ -103,3 +104,32 @@ http.listen(3000, function () {
     }, delay);
 
 });
+
+
+var dht_sensor = {
+    initialize: function () {
+        return sensorLib.initialize(22, 4);
+    },
+    read: function () {
+        var readout = sensorLib.read();
+        console.log('Temperature: ' + readout.temperature.toFixed(2) + 'C, ' +
+            'humidity: ' + readout.humidity.toFixed(2) + '%');
+        var json = JSON.stringify([  "Temperature: " + readout.temperature.toFixed(2) + "C, " +
+                      "humidity: " + readout.humidity.toFixed(2) + "%"  ]);
+        fs.appendFile('sensorlog.json', json, (err) => {
+            if(err) throw err;
+            console.log('appended');
+            
+        }
+                     );
+        setTimeout(function () {
+            dht_sensor.read();
+        }, 120000);
+    }
+};
+
+if (dht_sensor.initialize()) {
+    dht_sensor.read();
+} else {
+    console.warn('Failed to initialize sensor');
+}
